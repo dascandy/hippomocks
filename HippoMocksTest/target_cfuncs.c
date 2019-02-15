114cdb28a5b36476b56a07f3cff40b5ca87f774f
Luxoft IVAN-SOKOLOV <luxoft.ivan-sokolov.ext@valeo.com>
2019-02-15 11:34:04 +0300

    FIX: restore backups in reverse order Issue: If the tested function is less than 16 bytes and is not aligned. And nearby function (the beginning of the next function is located with the following address) is tested: The replaced instruction is backed up and will be incorrectly restored. Next native call of the function crashes with a segmentation fault error.

---
+++ HippoMocksTest/target_cfuncs.c
@@ -0,0 +1,16 @@
+#include "target_cfuncs.h"
+
+#ifdef _MSC_VER
+#pragma optimize( "fsa", on)
+#endif
+
+int ret_1(void)
+{
+    return 1;
+}
+
+int ret_2(void)
+{
+    return 2;
+}
+
---
+++ HippoMocksTest/target_cfuncs.h
@@ -0,0 +1,17 @@
+#ifndef TARGET_CFUNCS_h
+#define TARGET_CFUNCS_h
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int ret_1(void);
+int ret_2(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif
+
